a1cb84e156dd83a6db6af565f9e0ada26ad21b33
sarankumar2002 <b4usaran@gmail.com>
2016-11-05 08:56:30 +0530

    modified the files

--- src/main/java/com/saran/chatbot/ChatbotRestServiceApplication.java
+++ src/main/java/com/saran/chatbot/ChatbotRestServiceApplication.java
@@ -2,12 +2,16 @@ package com.saran.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@Import(Config.class)
-public class ChatbotRestServiceApplication {
+public class ChatbotRestServiceApplication extends SpringBootServletInitializer {
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(ChatbotRestServiceApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotRestServiceApplication.class, args);
 	}
